51833fe061a8b580dc7b113123a474189350369e
cfan310 <cfanning43@gmail.com>
2022-08-08 10:20:39 -0700

    update script.js

--- script.js
+++ script.js
@@ -17,11 +17,14 @@ buttonClick.addEventListener("click", showConjugationsOnClick);
 let mainTitle = document.getElementById("maintext");
 let changeTextButton = document.getElementById("mainTextButton");
 
+/*
 let newTitle = "French Verb Conjugator";
 
 function changeText() {
   mainTitle.innerHTML = newTitle;
 }
+*/
+
 
 changeTextButton.addEventListener("click", changeText);
 
