098b6bf32667fcbf81d7a5d212f16e64191be206
cfan310 <cfanning43@gmail.com>
2022-08-30 14:56:22 -0700

    add object code for conjugations in new js file

---
+++ conjugationobjs.js
@@ -0,0 +1,10 @@
+const toSpeak = {
+  present: "I speak",
+  "present progressive": "I am speaking",
+  "present perfect": "I have spoken",
+  preterite: "I spoke",
+  future: "I was speaking",
+  conditional: "I would speak",
+};
+
+console.log(toSpeak["present perfect"]);
