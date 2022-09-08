e43d7fe5725e5d92b7d37e9e03f5ee14b62a0d62
cfan310 <cfanning43@gmail.com>
2022-09-07 19:27:11 -0700

    add extra.js for code planning and testing

---
+++ extra.js
@@ -0,0 +1,75 @@
+// on click, show conjugations in a POP UP
+
+// <div class="answerPopUp" onclick="popUp()">
+
+// function popUp() {}
+
+/*
+let mainTitle = document.getElementById("maintext");
+let changeTextButton = document.getElementById("mainTextButton");
+
+
+let newTitle = "French Verb Conjugator";
+
+function changeText() {
+  mainTitle.innerHTML = newTitle;
+}
+
+
+changeTextButton.addEventListener("click", changeText);
+
+// Add the Spanish verb input as innerHTML in the div below
+
+var verbInput = document.getElementById("tense");
+
+var printedText = document.getElementById("verbInput");
+
+function addVerbToDiv() {
+  // now the printet text below the Conjugation: header will contain an English
+  // translation of the Spanish word typed into the input form
+  printedText == verbInput;
+}
+
+addVerbToDiv();
+
+// button test
+
+// Username and password page functionality
+
+// Flashcards functionality
+
+// Account page
+
+// next up is to create secure logins using cryptographic js data structure
+// add flashcards section template and js logic
+// css template before main js functionaity
+
+// adding css animation and google translate API key before
+// worrying about the conjugations
+
+
+// API config
+
+// Add account settings, then create general data structure to hold the 100 most common verbs.
+// Follow the above with a Google Translate API and get translation functionality.
+// Then add conjugations and format with CSS
+// Then add flashcard game Node.js backend
+
+
+const jsonData =
+  '{ "verb": { "name": "haber", "translation": "to speak", "conjugations": 7, "isCommon": true, "rating": null } }';
+
+const jsObject = JSON.parse(jsonData);
+
+console.log(jsObject);
+
+const verbTrans = jsObject.verb;
+
+console.log(verbTrans);
+
+console.log(verbTrans.name, verbTrans.translation, verbTrans.isCommon);
+
+const jsonReturn = JSON.stringify(jsObject); // return jsObject to original JSON form
+
+console.log(jsonReturn);
+*/
