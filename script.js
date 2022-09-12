39caa09c6b9d11b85066551af23079df4ffb5d4a
cfan310 <cfanning43@gmail.com>
2022-09-11 19:25:03 -0700

    added notes for sept 12th

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // USE CHROME BROWSER CONSOLE TO DEBUG
 /*
 const { json } = require("stream/consumers");
-*/
-//alert("SELECT LANGUAGE");
 
+//alert("SELECT LANGUAGE");
+var conjugationsFromAPI;
+let hiddenButton = document.getElementById("hidden-Button");
+*/
 let conjugateButton = document.getElementById("conjugate-Button");
 let conjugationsReveal = document.getElementById("conjugations");
 let verbTranslation = document.getElementById("verb-Translation");
@@ -11,19 +13,20 @@ let hideConjugation = document.getElementById("hide-Conjugations");
 
 //let conjugatedPopUp = document.getElementByClass("popUp");
 
-var conjugationsFromAPI;
+var popUp = document.getElementsByClassName("popup");
 
-let hiddenButton = document.getElementById("hidden-Button");
+const conjugationsTable = document.getElementById("conjugationsTable");
 
 // On Monday Sept 12th we need to make a popup appear with the hard-coded yo hablar conjugation and work from there
 // and store 10 common verbs into objects, then use prototype knowledge to take it further
 
 function showTable() {
-  conjugateButton.innerHTML(conjugationsTable);
-  conjugationsTable.style.display = "block";
-  verbTranslation.innerHTML = "hablar";
-  hiddenButton.style.display = "inline-block";
+  popUp.innerHTML = conjugationsTable;
+  //conjugationsTable.style.display = "block";
+  //hiddenButton.style.display = "inline-block";
 }
+
+conjugateButton.onclick = showTable();
 
 /*
 function showConjugationsOnClick() {
@@ -54,7 +57,7 @@ hiddenButton.addEventListener("click", hideButtonOnClick);
 
 // next step:
 // We now have the conjugations tale in html so here we add it to a js variable
-const conjugationsTable = document.getElementById("conjugationsTable");
+
 
 // linked to clickme function in above variable
 
@@ -62,7 +65,6 @@ const conjugationsTable = document.getElementById("conjugationsTable");
 // Let's make this table pop up
 
 // so for this
-// let conjugateButton = document.getElementById("conjugate-Button");
 
 // POPUP ON CLICK WITH THE conjugationsTable innerHTML
 
