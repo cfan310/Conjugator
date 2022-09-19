a887c2f683c3a9cfb0811e5a2f62e5550d03a515
cfan310 <cfanning43@gmail.com>
2022-09-19 13:14:44 -0700

    moved commented out code to  conjugationsobjs.js

--- conjugationobjs.js
+++ conjugationobjs.js
@@ -33,3 +33,48 @@ var poner = new Conjugations();
 console.log(hablar.present);
 
 // find best object structure; hard code at first, then connect API, then re-code on top of API GET requests for conjugations in modal window
+
+/*
+function showConjugationsOnClick() {
+  // function to reveal hard-coded conjugation example
+  conjugationsReveal.style.display = "block";
+  verbTranslation.innerHTML = "hablar"; // trobuleshoot why this hard-coded poner doesn't show up
+  conjugationsReveal.innerHTML = "hablar = to speak";
+  hiddenButton.style.display = "inline-block"; // and we will also reveal the "hide" button
+}
+
+conjugateButton.addEventListener("click", showConjugationsOnClick);
+// activate show conjugations function after clicking the conjugate button
+
+
+function hideConjugationsOnClick() {
+  conjugationsReveal.style.display = "none";
+  verbTranslation.style.display = "none";
+  hideConjugation.style.display = "none";
+}
+hideConjugation.addEventListener("click", hideConjugationsOnClick);
+
+function hideButtonOnClick() {
+  hiddenButton.style.display = "none";
+}
+
+hiddenButton.addEventListener("click", hideButtonOnClick);
+
+
+// next step:
+// We now have the conjugations tale in html so here we add it to a js variable
+
+
+// linked to clickme function in above variable
+
+
+// Let's make this table pop up
+
+// so for this
+
+// POPUP ON CLICK WITH THE conjugationsTable innerHTML
+
+// Add hard-coded conjugations to CSS styled pop-up window upon click.
+// The popup window will hold the hide button
+// restructure the HTML into divs
+*/
